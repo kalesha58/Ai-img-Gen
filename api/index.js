f7d01ccc1866866ec0b8d13a58c8705528ac5e31
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2023-02-25 08:11:11 +0530

    final-test:)

--- api/index.js
+++ api/index.js
@@ -8,7 +8,11 @@ import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config({ path:"api/.env"});
 
-const app = express();
+
+
+
+
+const app = express()
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({extended:false}))
